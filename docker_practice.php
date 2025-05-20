53b905b36076f07d6c6ba94698183924a1efad1f
Ryusei-Suzuki <torichan1207@gmail.com>
2025-05-20 12:53:23 +0900

    dockerコマンド記載用ファイル作成

---
+++ docker_practice.php
@@ -0,0 +1,16 @@
+<?php
+// $ docker pull centos:centos7 //docker imageをローカルへ持ってくる
+// $ docker images //ローカルに持ってきたdocker imageを表示
+// $ docker run -it -d --name centos7 centos:centos7 //仮想環境の起動
+// $ docker ps //仮想環境のステータス確認（STATUSの項目にUpと表記されていれば起動中） 
+// $ docker exec -it centos7 bash //仮想環境へログイン
+// $ exit //仮想環境から出る
+// $ docker stop centos7 //コンテナの停止
+// $ docker start centos7 //コンテナの再稼働
+// $ docker-compose up -d //コンテナの複数立ち上げ
+// $ docker-compose exec [サービス名] bash //コンテナ内で作業
+// $ docker-compose down //コンテナ一括削除
+
+
+
+
